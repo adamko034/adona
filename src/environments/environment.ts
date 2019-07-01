91d3ed7ec2e2190b246b8da53ff5d5d496443a27
Adam Kotas <adamko034@outlook.com>
2019-07-01 21:56:25 +0200

    travis configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCpb-IuvyErf7LZl904UI9hgFt118_1G40",
+    authDomain: "adona-73208.firebaseapp.com",
+    databaseURL: "https://adona-73208.firebaseio.com",
+    projectId: "adona-73208",
+    storageBucket: "",
+    messagingSenderId: "113224921251",
+    appId: "1:113224921251:web:188b37b8fa2e4a62"
+  }
 };
 
 /*
