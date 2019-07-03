eeb7829eca985f8087b835706573f069b4372eea
Adam Kotas <adamko034@outlook.com>
2019-07-03 22:11:52 +0200

    calendar - created module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'calendar',
+    loadChildren: () => import('./calendar/calendar.module').then(mod => mod.CalendarModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
