208c8a0389c75a11dd48faa2a7d466163c928aba
Adam Kotas <adamko034@outlook.com>
2020-01-16 23:02:44 +0100

    feat: teams - auth store new version

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
+import { MatDialogModule, MatStepperModule } from '@angular/material';
+import { SharedModule } from '../../shared/shared.module';
+import { NewTeamDialogComponent } from './components/dialogs/new-team-dialog/new-team-dialog.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent, NewTeamDialogComponent],
+  imports: [CommonModule, SharedModule, MatStepperModule, MatDialogModule, HomeRoutingModule],
+  entryComponents: [NewTeamDialogComponent]
 })
-export class HomeModule { }
+export class HomeModule {}
--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthFacade } from '../../../../core/auth/auth.facade';
+import { User } from '../../../../core/auth/model/user-model';
+import { DialogService } from '../../../../shared/services/dialogs/dialog.service';
+import { NewTeamDialogComponent } from '../../components/dialogs/new-team-dialog/new-team-dialog.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private userSubscription: Subscription;
 
-  constructor() { }
+  public user: User;
 
-  ngOnInit() {
+  constructor(private authFacade: AuthFacade, private dialogService: DialogService) {}
+
+  public ngOnInit() {
+    this.userSubscription = this.authFacade.getUser().subscribe((user: User) => {
+      this.user = user;
+    });
+
+    if (!this.userHasTeams()) {
+      this.dialogService.open(NewTeamDialogComponent);
+    }
   }
 
+  public ngOnDestroy() {
+    if (this.userSubscription) {
+      this.userSubscription.unsubscribe();
+    }
+  }
+
+  public userHasTeams(): boolean {
+    return this.user && this.user.teams && this.user.teams.length > 0;
+  }
 }
